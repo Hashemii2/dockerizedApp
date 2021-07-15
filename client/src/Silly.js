0d67a658b26e918be447c9e8f42be96cbeb68577
mahoor <mmahoor59@yahoo.com>
2021-07-15 15:01:38 +0430

    change in client

--- client/src/Silly.js
+++ client/src/Silly.js
@@ -3,23 +3,31 @@ import axios from "axios";
 
 class silly extends Component {
   state = {
-    text: {},
+    data: [],
   };
 
   componentDidMount() {
-    this.fetchtext();
+    this.fetchdata();
   }
 
-  async fetchtext() {
-    const text = await axios.get("/api/");
-    this.setState({ text: text.data });
-  }
+  fetchdata = async () => {
+    try {
+      const res = await axios.get("http://localhost:5000/api");
+      this.setState({ data: res.data });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   render() {
+    const data = this.state.data;
+
     return (
       <div>
-        <h3>From node js</h3>
-        {this.fetchtext()}
+        <h3>this data is From node js:</h3>
+        {data.map((d) => (
+          <div>{d.name}</div>
+        ))}
       </div>
     );
   }
--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("hello");
+let data = [
+  {
+    id: "1",
+    name: "faezeh",
+    lastname: "hashemi",
+    info: "This info is from node js",
+  },
+  {
+    id: "2",
+    name: "ali",
+    lastname: "karimi",
+    info: "This info is from node js",
+  },
+
+  {
+    id: "3",
+    name: "mona",
+    lastname: "hatami",
+    info: "This info is from node js",
+  },
+];
+
+app.get("/api", (req, res) => {
+  res.send(data);
 });
 
 //not add mongodb yet.. later i expand this project...
